2ca1d988b31bf1b967eb71c655f7ba77867541e4
gudvin <gin_tolik@mail.ru>
2023-11-16 23:34:18 +0300

    added yandexdriver

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -5,6 +5,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,9 +14,25 @@ abstract public class BaseTest {
 
     @Before
     public void setUp(){
-        WebDriverManager.chromedriver().driverVersion("119.0.6045.123").setup();
+//        WebDriverManager.chromedriver().driverVersion("119.0.6045.123").setup();
+//
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+//        driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
+//        BasePage.setDriver(driver);
+//        driver.get("https://stellarburgers.nomoreparties.site/");
 
-        driver = new ChromeDriver();
+//         for testing in Yandex Browser
+        WebDriverManager.chromedriver().driverVersion("src/main/resources/yandexdriver").setup();
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("/Applications/Yandex.app/Contents/MacOS/Yandex");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--disable-dev-shm-usage");// overcome limited resource problems
+//        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
